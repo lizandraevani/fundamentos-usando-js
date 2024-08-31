c18528c41ab0766d3c57fd2c6b5f819d024d186b
lizandraevani <lizandra.nascimento@discentes.fat.uerj.br>
2024-08-30 21:29:44 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,7 @@
+//Declare duas variásveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores no console//
+
+var idade = 26;
+var peso = 58;
+
+
+console.log("olá, você tem  "+idade+" anos e o seu peso é de  "+peso+ "kg")
---
+++ script.js
@@ -0,0 +1,8 @@
+const cidade="resende";
+console.log(cidade);
+
+//Declare duas variásveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores no console//
+
+
+var idade = "idade"
+var peso = "peso"
